bf1bff143e94005167f1b03db1bd0c23bbdfce89
Dawnfz-Lenfeng <2912706234@qq.com>
2024-11-17 23:26:54 +0800

    fix username

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from .documents import router as documents_router
-from .keywords import router as keywords_router
-from .graph import router as graph_router
+# from .keywords import router as keywords_router
+# from .graph import router as graph_router
 
 api_router = APIRouter()
 
-api_router.include_router(documents_router, prefix="/documents", tags=["documents"])
-api_router.include_router(keywords_router, prefix="/keywords", tags=["keywords"])
-api_router.include_router(graph_router, prefix="/graph", tags=["graph"])
+api_router.include_router(documents_router)
+# api_router.include_router(keywords_router, prefix="/keywords", tags=["keywords"])
+# api_router.include_router(graph_router, prefix="/graph", tags=["graph"])
