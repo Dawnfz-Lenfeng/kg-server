dfa0c306e12f63e0a92610bd32145edc590a5b88
Dawnfz-Lenfeng <2912706234@qq.com>
2024-11-18 21:23:53 +0800

    modify router of keywords

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -8,5 +8,5 @@ from .keywords import router as keywords_router
 api_router = APIRouter()
 
 api_router.include_router(documents_router)
-api_router.include_router(keywords_router, prefix="/keywords", tags=["keywords"])
+api_router.include_router(keywords_router)
 # api_router.include_router(graph_router, prefix="/graph", tags=["graph"])
